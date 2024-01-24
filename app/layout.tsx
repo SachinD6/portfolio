4a4462b449a9daba83dded4869b46978849e0b1f
Sachin Chaudhary <sachinduhan1223@gmail.com>
2024-01-24 11:37:18 +0530

    added vercel insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import { GeistSans } from 'geist/font/sans';
 import { GeistMono } from 'geist/font/mono';
 
 import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 
 import "./globals.css";
